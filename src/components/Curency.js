0cab2c94d556b31c393f2446aee93d7d38a1dbeb
Grozdanny <marcinzur277@gmail.com>
2021-10-06 12:55:03 +0000

    I DID IT ITS NOW TEMP THO

--- src/components/Curency.js
+++ src/components/Curency.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM, { render } from 'react-dom'
 
-
-class CurencyChange extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            Pln: 0,
-            Sek: 0,
-        };
-    }
-
-    changeCurency(value,scale){
-        switch(scale){
-            case "Sek":
-                this.setState({Pln: value * 0.45, Sek: value});
-                return;
-            case "Pln":
-                this.setState({Sek: value * 2.2, Pln: value});
-                return;
-        }
-    }
-
-    render(){
-        return<>
-        <p>Pln</p>
-        <input type="text" value={this.state.Pln} onChange={(event) => this.changeCurency(event.target.value, "Pln") }/>
-        <p>Sek</p>
-        <input type="text" value={this.state.Sek} onChange={(event) => this.changeCurency(event.target.value, "Sek") }/>
-        </>
-    }
+class Curency extends React.Component {
+    constructor(props) {
+     super(props);
 }
-export default CurencyChange;
+
+render() {
+    return (
+      <>
+        <p>{this.props.curencyName.toUpperCase()}</p>
+        <input
+          type="text"
+          value={this.props.curencyValue}
+          onChange={(event) => {
+            this.props.changeCurency(
+              event.target.value,
+              this.props.curencyName
+            );
+          }}
+        />
+      </>
+    );
+  }
+}
+export default Curency;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import CurencyChange from './components/Curency';
+import Curency from './components/Curency';
 
-   const Moneyy = (
-       <>
-       <CurencyChange />
-       </>
-   )
-    ReactDOM.render(Moneyy, document.getElementById('root'));
+class Exchange extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+        Pln: 0,
+        Sek: 0,
+    };
+
+this.changeCurency = this.changeCurency.bind(this);
+}
+
+changeCurency(value, scale){
+    switch(scale){
+        case "Sek":
+            this.setState({Pln: value * 0.45, Sek: value});
+            return;
+        case "Pln":
+            this.setState({Sek: value * 2.2, Pln: value});
+            return;
+    }
+}
+render() {
+    return (
+        <main>
+         <Curency
+          curencyName="Sek"
+          curencyValue={this.state.Sek}
+          changeCurency={this.changeCurency}
+        />
+
+        <Curency
+         curencyName="Pln"
+         curencyValue={this.state.Pln}
+         changeCurency={this.changeCurency}
+        />
+        </main>
+    )
+    
+}
+}
+
+ReactDOM.render(<Exchange/>, document.getElementById('root'));
 
